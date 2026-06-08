1e52315cdba430a0ce46a8294e0937f3120e1adc
fi.ceflot <fikreerp@gmail.com>
2026-06-08 13:00:23 +0200

    14

--- android/app/src/main/java/com/ceflot/siteapp/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.ceflot.siteapp;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- shims/node-domexception/index.js
+++
@@ -1,2 +0,0 @@
-// Native DOMException shim to replace the deprecated node-domexception package
-module.exports = globalThis.DOMException;
